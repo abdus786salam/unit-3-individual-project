c7d465af7abdff7c85e1448367bba211d5a64965
abdus786salam <abdus786salam@gmail.com>
2022-06-14 18:28:03 +0530

    listing functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
-document.getElementById('dropdown-menu').onclick="dropdownFunction"
-let dropdownFunction=()=>{
+document.getElementById('header_div_right_button').addEventListener( 'click',()=>{
 
-
-
+event.preventDefault()
+let menu=document.querySelector('.dropdown_menu')
+if (menu.style.display == "none") {
+    menu.style.display = "block"
+} else {
+    menu.style.display = "none"
 }
+ 
+
+})
+
+
+
+
