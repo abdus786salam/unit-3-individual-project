e59b5a83c7e62a16fc72bdd3f6129cf2306583cd
abdus786salam <abdus786salam@gmail.com>
2022-06-16 09:40:49 +0530

    landing page updated

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,34 @@ document.getElementById('header_div_right_button').addEventListener( 'click',()=
 
 event.preventDefault()
 let menu=document.querySelector('.dropdown_menu')
-if (menu.style.display == "none") {
-    menu.style.display = "block"
-} else {
+if (menu.style.display == "block") {
     menu.style.display = "none"
+} else {
+    menu.style.display = "block"
 }
- 
-
 })
+
+document.getElementById('signin').addEventListener( 'click',()=>{
+
+    event.preventDefault()
+    let menu=document.querySelector('#signin-info')
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "block"
+    }
+    })
+    document.getElementById('div-date-input-location').addEventListener( 'click',()=>{
+
+        event.preventDefault()
+        let menu=document.querySelector('#loation-list')
+        if (menu.style.display == "block") {
+            menu.style.display = "none"
+        } else {
+            menu.style.display = "block"
+        }
+        })
+  
 
 
 
